61e6c96bb38b239bbd007f12044f934a6b06f3df
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-08-05 13:07:54 +0900

    refactor

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -37,44 +37,48 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		if line == "pwd" {
-			pwd, err := helpers.GetCurrDir()
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			fmt.Println(pwd)
-		} else if line == "exit" {
-			fmt.Println("Goodbye!")
-			os.Exit(0)
-		} else if strings.HasPrefix(line, "cd") {
-			parts := strings.SplitN(line, " ", 2)
-			if len(parts) != 2 {
-				fmt.Fprintf(os.Stderr, "Invalid cd command\n")
-				continue
-			}
-			path := parts[1]
-			if path[0] != '/' {
-				currentDir, err := helpers.GetCurrDir()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Failed to change dir\n")
-					continue
-				}
-				path = fmt.Sprintf("%s/%s", currentDir, path)
-			}
-			err = helpers.ChangeDir(path)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error: %s\n", err)
-				continue
-			}
-		} else if strings.HasPrefix(line, "echo") {
-			parts := strings.SplitN(line, " ", 2)
-			if len(parts) > 1 {
-				fmt.Println(parts[1])
-			}
-		} else {
-			res := fmt.Sprintf("%s is not a valid command", line)
-			fmt.Println(res)
+		handleCommand(line)
+	}
+}
+
+func handleCommand(line string) {
+	if line == "pwd" {
+		pwd, err := helpers.GetCurrDir()
+		if err != nil {
+			log.Println(err)
+			return
 		}
+		fmt.Println(pwd)
+	} else if line == "exit" {
+		fmt.Println("Goodbye!")
+		os.Exit(0)
+	} else if strings.HasPrefix(line, "cd") {
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) != 2 {
+			fmt.Fprintf(os.Stderr, "Invalid cd command\n")
+			return
+		}
+		path := parts[1]
+		if path[0] != '/' {
+			currentDir, err := helpers.GetCurrDir()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to change dir\n")
+				return
+			}
+			path = fmt.Sprintf("%s/%s", currentDir, path)
+		}
+		err := helpers.ChangeDir(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			return
+		}
+	} else if strings.HasPrefix(line, "echo") {
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) > 1 {
+			fmt.Println(parts[1])
+		}
+	} else {
+		res := fmt.Sprintf("%s is not a valid command", line)
+		fmt.Println(res)
 	}
 }
