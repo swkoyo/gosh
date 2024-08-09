b7eb0332c3fa79af0232bab73b4330469f6631f3
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-08-09 16:40:59 +0900

    add redirection

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -9,6 +9,8 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"golang.org/x/term"
 )
 
 func main() {
@@ -30,18 +32,84 @@ func main() {
 		}
 	}()
 
-	for {
-		fmt.Print(ps1)
-		line, err := inputReader.ReadString('\n')
-		line = line[:len(line)-1]
-		if err != nil {
+	if term.IsTerminal(int(os.Stdin.Fd())) {
+		for {
+			fmt.Print(ps1)
+			line, err := inputReader.ReadString('\n')
+			line = strings.TrimSpace(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+			handleCommand(line)
+		}
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			handleCommand(line)
+		}
+		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
-		handleCommand(line)
 	}
 }
 
 func handleCommand(line string) {
+	if strings.Contains(line, ">") || strings.Contains(line, "<") {
+		parts := strings.Fields(line)
+		var cmd, inputFile, outputFile string
+
+		for i := 0; i < len(parts); i++ {
+			if parts[i] == ">" {
+				if i+1 < len(parts) {
+					outputFile = parts[i+1]
+					break
+				}
+			} else if parts[i] == "<" {
+				if i+1 < len(parts) {
+					inputFile = parts[i+1]
+					break
+				}
+			} else {
+				cmd += parts[i] + " "
+			}
+		}
+
+		cmd = strings.TrimSpace(cmd)
+
+		if inputFile != "" {
+			file, err := os.Open(inputFile)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s\n", err)
+				return
+			}
+			defer file.Close()
+			os.Stdin = file
+		}
+
+		if outputFile != "" {
+			file, err := os.Create(outputFile)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s\n", err)
+				return
+			}
+			defer file.Close()
+			os.Stdout = file
+		}
+
+		handleCommand(cmd)
+
+		if inputFile != "" {
+			os.Stdin = os.NewFile(uintptr(syscall.Stdin), "/dev/stdin")
+		}
+
+		if outputFile != "" {
+			os.Stdout = os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
+		}
+
+		return
+	}
+
 	if line == "pwd" {
 		pwd, err := helpers.GetCurrDir()
 		if err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module gosh
 
 go 1.22.4
+
+require golang.org/x/term v0.23.0
+
+require golang.org/x/sys v0.23.0 // indirect
